6f08643090b755b99c9a11f9bd0a29fae06b0537
Lenny <lenny@hothouselabs.com>
2013-05-31 17:57:18 -0700

    \#import <MailCore.h> // from mailcore2 :)

---
+++ Build Test/TSTObject.h
@@ -0,0 +1,13 @@
+//
+//  TSTObject.h
+//  Build Test
+//
+//  Created by Build Master on 5/31/13.
+//  Copyright (c) 2013 HH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TSTObject : NSObject
+
+@end
---
+++ Build Test/TSTObject.m
@@ -0,0 +1,14 @@
+//
+//  TSTObject.m
+//  Build Test
+//
+//  Created by Build Master on 5/31/13.
+//  Copyright (c) 2013 HH. All rights reserved.
+//
+
+#import "TSTObject.h"
+#import <MailCore.h>
+
+@implementation TSTObject
+
+@end
